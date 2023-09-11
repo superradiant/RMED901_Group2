9d47d174c24874eff78f378923c863f7bceb530c
“potatoman” <jan-ake.husmann@uib.no>
2023-09-11 12:04:42 +0200

    Addition of easy calculaiton

--- 20230911_collaboration_test.R
+++ 20230911_collaboration_test.R
@@ -5,3 +5,7 @@
 # Description: Let's work here togethers
 # Project: RMED901
 #--------------------------------------------####
+
+x <- c(3, 6, 2, NA) 
+mean(x, na.rm = T) # na.rm defalt = FALSE, so it throws error
+sd(x, na.rm = T)
