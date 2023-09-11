58e6ebcd045ea81eb356e57e8dc86b1dd1bc6c4b
“potatoman” <jan-ake.husmann@uib.no>
2023-09-11 12:10:51 +0200

    Copied an old function in the script

--- 20230911_collaboration_test.R
+++ 20230911_collaboration_test.R
@@ -9,3 +9,23 @@
 x <- c(3, 6, 2, NA) 
 mean(x, na.rm = T) # na.rm defalt = FALSE, so it throws error
 sd(x, na.rm = T)
+
+check_input <- function(
+    argument1, # no default: this argument is required!
+    argument2 = 2, # 2 is default
+    argument3 = 'a' # 'a' is default
+){
+  if(argument1 > 5){
+    warning("Argument1 should be <= 5, assuming 5.")
+    argument1 <- 5
+  }
+  if(!is.numeric(argument2)){
+    stop("Argument2 should be numeric!")
+  }
+  if(is.character(argument3)){
+    message("Well done!")
+  }
+  output <- list(argument1, argument2, argument3)
+  return(output)
+}
+check_input(1)
