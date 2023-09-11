045a0157ab5ab7f15608d8bfcba33b29a4d5e960
ingvildbrus <ingvildsbruserud@gmail.com>
2023-09-11 12:22:26 +0200

    We test

---
+++ 20230911_collaboration_test.R
@@ -0,0 +1,10 @@
+#-------------Script Header------------------####
+# Date: 2023-09-11      
+# Author: jan-ake
+# Filename: Test_scriptbs
+# Description: Let's work here togethers
+# Project: RMED901
+#--------------------------------------------####
+
+
+project<-jippi
