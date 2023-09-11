be8cb00916674d2628a4991be71b2f7c004f30eb
“potatoman” <jan-ake.husmann@uib.no>
2023-09-11 11:53:50 +0200

    Small changes

--- 20230911_collaboration_test.R
+++ 20230911_collaboration_test.R
@@ -1,1 +1,7 @@
-asdf
+#-------------Script Header------------------####
+# Date: 2023-09-11      
+# Author: jan-ake
+# Filename: Test_scriptbs
+# Description: Let's work here togethers
+# Project: RMED901
+#--------------------------------------------####
